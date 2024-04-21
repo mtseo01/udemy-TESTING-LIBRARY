4614f79ee2d29050efca5435aac1b9a314d6e630
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-04-21 19:31:13 +0900

    refactor: 쿼리 변경 ( getByText -> getByRole )

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("App contains correct heading", () => {
+test('App contains correct heading', () => {
   render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
+  const headingElement = screen.getByRole('heading', { name: /learn react/i });
   expect(headingElement).toBeInTheDocument();
 });
